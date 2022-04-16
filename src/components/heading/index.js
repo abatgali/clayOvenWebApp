e6ece2332b2959c04c38759e1ae01d715ea07433
Shivam <shivam@MacBook-Pro.local>
2022-04-17 02:48:38 +0530

    converting to next js setup

--- src/components/heading/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-const Heading = ({ heading }) => {
-  return <h1 className={styles.heading}>{heading}</h1>;
-};
-
-export default Heading;
