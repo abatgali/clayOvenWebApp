934f0662cca0a968b4192dd09f55cbca3fd48460
abatgali <abatgali@iu.edu>
2022-05-30 18:14:50 -0400

    GET items of a category endpoint added

--- pages/api/categories/[id]/items.js
+++ pages/api/categories/[id]/items.js
@@ -1,9 +1,24 @@
+// returns items of a specific category
+
 import { PrismaClient } from '@prisma/client';
-// import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    const items = await prisma.items.getItemById(req.query.id);
+
+    // retrieving uri id param from request
+    const param = parseInt(req.query.id);
+
+    // using orm to retrieve related items
+    const items = await prisma.live_menu.findMany({
+        select: {
+            items: true,
+            categories: true
+        },
+        where: {
+            category_id: param
+        }
+    });
+
     res.json(items);
 }
--- pages/api/items/[id].js
+++ pages/api/items/[id].js
@@ -1,11 +1,14 @@
-// returns all items
+// returns a specific item
 
 import { PrismaClient } from '@prisma/client';
-// import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    const items = await prisma.items.findMany();
+    const items = await prisma.items.findUnique({
+        where: {
+            item_id: parseInt(req.query.id)
+        }
+    });
     res.json(items);
 }
